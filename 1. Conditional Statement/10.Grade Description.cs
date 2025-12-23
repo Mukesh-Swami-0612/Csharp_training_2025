53f83ab6af0d7ed9bb8fc88a3cfa4a9c3e256431
Mukesh Kumar <121216988+Mukesh-Swami-0612@users.noreply.github.com>
2025-12-23 23:17:04 +0530

    Update 10.Grade Description.cs

--- 1. Conditional Statement/10.Grade Description.cs
+++ 1. Conditional Statement/10.Grade Description.cs
@@ -1,34 +1,45 @@
-
 // Question:
 // Input grade (E, V, G, A, F) and print description
 
 using System;
+// Using System namespace for Console input/output
 
 class GradeDescription
 {
+    // Program execution starts from Main method
     static void Main()
     {
+        // Ask user to enter grade
         Console.Write("Enter grade (E/V/G/A/F): ");
+
+        // Read input, take first character, convert it to uppercase
+        // This avoids issues if user enters lowercase letters
         char grade = char.ToUpper(Console.ReadLine()[0]);
 
+        // Switch statement to match grade value
         switch (grade)
         {
-            case 'E':
+            case 'E':   // If grade is E
                 Console.WriteLine("Excellent");
                 break;
-            case 'V':
+
+            case 'V':   // If grade is V
                 Console.WriteLine("Very Good");
                 break;
-            case 'G':
+
+            case 'G':   // If grade is G
                 Console.WriteLine("Good");
                 break;
-            case 'A':
+
+            case 'A':   // If grade is A
                 Console.WriteLine("Average");
                 break;
-            case 'F':
+
+            case 'F':   // If grade is F
                 Console.WriteLine("Fail");
                 break;
-            default:
+
+            default:    // If grade does not match any case
                 Console.WriteLine("Invalid Grade");
                 break;
         }
