a688b0ffd93575a78937f0a574f1bb0fe21f86a9
Mukesh Kumar <121216988+Mukesh-Swami-0612@users.noreply.github.com>
2025-12-23 23:23:25 +0530

    Update 10.Guessing Game.cs

--- 2. Loops and Jump Statements/10.Guessing Game.cs
+++ 2. Loops and Jump Statements/10.Guessing Game.cs
@@ -1,23 +1,35 @@
-
 // Problem:
 // Guess the secret number using do-while loop
 
 using System;
+// Using System namespace for Console input/output
 
 class GuessingGame
 {
+    // Program execution starts from Main method
     static void Main()
     {
+        // Secret number to be guessed
         int secret = 7;
+
+        // Variable to store user's guess
         int guess;
 
+        // do-while loop
+        // This loop runs at least once
         do
         {
+            // Ask user to guess the number
             Console.Write("Guess the number: ");
+
+            // Read user's guess
             guess = int.Parse(Console.ReadLine());
 
-        } while (guess != secret);
+        } 
+        // Loop continues until guess equals secret number
+        while (guess != secret);
 
+        // This line runs when correct guess is made
         Console.WriteLine("You guessed it right!");
     }
 }
